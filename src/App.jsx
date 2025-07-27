024790fbb2ff6ff3593fa329d70f303f0f19ff70
Alok <alok78555@gmail.com>
2025-07-27 17:07:15 +0530

    change Could not resolve ./components/callChatBot from src/App.jsx to  import CallChatBot from './components/CallChatBot';

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-
 import { useState } from 'react'
 import Home from './pages/Home'
 import HeroPage from './pages/HeroPage'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CallChatBot from './components/callChatBot';
+import CallChatBot from './components/CallChatBot';  // <- FIXED CASE
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
-   <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/MediBot" element={<CallChatBot/>} />
-
-      
-
-     </Routes>
-   </BrowserRouter>
-
-
-   
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/MediBot" element={<CallChatBot />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
